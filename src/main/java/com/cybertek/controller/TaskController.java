a2b605dad8ef87a8c956b1cf3eb74954bf724936
ozzy <ozzy@cybertekschool.com>
2020-10-06 12:24:52 -0400

    Task Archive is completed

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cybertek.data.DataGenerator;
 import com.cybertek.dto.TaskDTO;
+import com.cybertek.dto.UserDTO;
 import com.cybertek.implementation.ProjectServiceImpl;
 import com.cybertek.implementation.TaskServiceImpl;
 import com.cybertek.implementation.UserServiceImpl;
@@ -112,6 +114,73 @@ public class TaskController {
 		return "task/assign";
 	}
 	
+	///////////////////////
+	
+	@GetMapping("/employee/taskStatus")
+	public String getStatusOfTask(Model model) {
+
+		UserDTO user = DataGenerator.activeUser;
+		List<TaskDTO> tasks = taskService.getEmployeeTasks(user);
+
+		model.addAttribute("tasks", tasks);
+		model.addAttribute("task", new TaskDTO());
+		
+		model.addAttribute("statuses", DataGenerator.getStatusList());
+		model.addAttribute("users", userService.listOfUserDTO());
+		model.addAttribute("projects",projectService.getListOfProjectDTO() );
+
+		return "task/task-status";
+	}
+	
+	@GetMapping("/employee/update/{id}")
+	public String editTaskStatus(@PathVariable("id") Long id, Model model) {
+
+		UserDTO user = DataGenerator.activeUser;
+		List<TaskDTO> tasks = taskService.getEmployeeTasks(user);
+
+		TaskDTO task =  taskService.getTaskDTOById(id);
+
+
+		model.addAttribute("tasks", tasks);
+
+		model.addAttribute("task", task);
+		model.addAttribute("users", userService.listOfUserDTO());
+		model.addAttribute("projects", projectService.getListOfProjectDTO());
+		model.addAttribute("statuses", DataGenerator.getStatusList());
+		return "task/task-update";
+	}
+	
+	
+	@PostMapping("/employee/update/{id}")
+	public String updateTaskStatus(@PathVariable("id") Long id, @ModelAttribute("task") TaskDTO taskDTO,Model model) {
+
+		UserDTO user = DataGenerator.activeUser;
+		
+		List<TaskDTO> tasks =taskService.updateTaskDTOForEmployee(user,taskDTO);
+
+		model.addAttribute("tasks", tasks);
+		
+		model.addAttribute("task", taskDTO);
+		model.addAttribute("statuses", DataGenerator.getStatusList());
+		model.addAttribute("users", userService.listOfUserDTO());
+		model.addAttribute("projects", projectService.getListOfProjectDTO());
+
+		return "task/task-update";
+	}
+	
+	////////////////////
+	
+	@GetMapping("/employee/archive")
+	public String getCompletedTasks(Model model) {
+
+		UserDTO user = DataGenerator.activeUser;
+		
+		List<TaskDTO> tasks = taskService.archiveList(user);
+				
+		model.addAttribute("tasks", tasks);
+
+		return "task/task-archive";
+	}
 	
 
 }
